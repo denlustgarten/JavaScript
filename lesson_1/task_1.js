06d4fdeb75c83acac6cc90600ccf106f5ecc6d25
Denis_Lustgarten <denlust@yandex.ru>
2022-09-08 23:34:25 +0300

    added lesson_1

---
+++ lesson_1/task_1.js
@@ -0,0 +1,6 @@
+"use strict";
+let celsius_temp = 23;
+let fahrenheit_temp = 0
+fahrenheit_temp = (9 / 5) * celsius_temp + 32;
+console.log("Цельсия: " + celsius_temp + " °C\n" +
+    "Фаренгейта: " + fahrenheit_temp + " °F")

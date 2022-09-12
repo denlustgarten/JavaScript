1fb9f6495bd9de219e8b52ca0e6cfdb08e7420db
Denis_Lustgarten <denlust@yandex.ru>
2022-09-12 13:55:35 +0300

    upd task_1

--- lesson_3/task_1.js
+++ lesson_3/task_1.js
@@ -1,6 +1,6 @@
-for (var i = 2; i < 100; i++) {
-    j = 2;
-    is_simple = 1;
+for (let i = 2; i < 100; i++) {
+    let j = 2;
+    let is_simple = 1;
     while (j <= i / 2) {
         if (i % j == 0) {
             is_simple = 0;
